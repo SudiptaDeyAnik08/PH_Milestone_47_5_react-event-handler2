4ea54113c77e61b7b973bf82277ae1b648b143da
SudiptaDeyAnik08 <sudiptadeysd06@gmail.com>
2023-02-01 00:37:51 +0600

    commit

--- src/components/Shoes/Shoes.js
+++ src/components/Shoes/Shoes.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import  { add,mul } from '../utilities/Calculate'
 
 function Shoes(){
+    const a = 10;
+    const b = 20;
+    const total = add(a,b);
+    const multiplication = mul(a,b);
     return(
         <div>  
             <p>Welcome to our shoe store</p>
+            <p>Total Summation: {total}</p>
+            <p>Total Multiplication: {multiplication}</p>
+
         </div>
     )
 }
---
+++ src/components/utilities/Calculate.js
@@ -0,0 +1,11 @@
+const add= (first,second) =>{
+    return first+second;
+}
+
+
+
+const mul = (a,b)=>{
+    return a*b;
+}
+
+export {add,mul}
